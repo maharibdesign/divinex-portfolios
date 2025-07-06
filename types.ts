bffa5c4a21e538b02f64d282cf9d106f6f952494
maharibdesign <berekettsparkpro@gmail.com>
2025-07-06 09:51:10 +0000

    fix: Implement strict type safety for product data

--- types.ts
+++ types.ts
@@ -1,4 +1,5 @@
-// This file defines the shape of our Product data.
+// This file defines the explicit shape of our Product data.
+// It acknowledges that some fields from the database might be null.
 
 export interface Product {
   id: number;
@@ -8,6 +9,6 @@ export interface Product {
   category: string | null;
   price: number | null;
   image: string | null;
-  // We explicitly type 'images' as an array of strings.
+  // We explicitly type 'images' as an array of strings that could also be null.
   images: string[] | null; 
 }
